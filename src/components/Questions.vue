7e1e04b314f028e500080a6db8dc221a217aa14a
bourbakiii <gabisov03912@gmail.com>
2023-05-31 16:48:33 +0300

    .

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -4,14 +4,14 @@
       FAQ
     </small>
     <h3 class="questions__title">Answers to our frequently asked questions</h3>
-    <div class="question__content">
-      <div class="question">
-        <button class="question__button">
-          What is Tailwind CSS?
+    <div class="questions__content">
+      <div v-for="(question,index) in questions" class="question" :class="{opened: question.is_opened}">
+        <button @click.prevent="question.is_opened = !question.is_opened" class="question__button">
+          {{ question.title }}
+          <icon class="question__arrow" name="question-arrow"/>
         </button>
         <div class="question__content">
-          Tailwind CSS is a CSS framework like Bootstrap, that speeds up development time for developers. Tailwind CSS
-          uses classes to style elements, meaning you do not have to write any CSS.
+          {{ question.text }}
         </div>
       </div>
     </div>
@@ -19,6 +19,36 @@
 </template>
 
 <script setup>
+import Icon from "@/components/Icon.vue";
+import {reactive} from "vue";
+
+const questions = reactive([
+  {
+    title: "What is Tailwind CSS?",
+    text: "Tailwind CSS is a CSS framework like Bootstrap, that speeds up development time for developers. Tailwind CSS uses classes to style elements, meaning you do not have to write any CSS.",
+    is_opened: false
+  },
+  {
+    title: "What is Tailwind CSS?",
+    text: "Tailwind CSS is a CSS framework like Bootstrap, that speeds up development time for developers. Tailwind CSS uses classes to style elements, meaning you do not have to write any CSS.",
+    is_opened: false
+  },
+  {
+    title: "What is Tailwind CSS?",
+    text: "Tailwind CSS is a CSS framework like Bootstrap, that speeds up development time for developers. Tailwind CSS uses classes to style elements, meaning you do not have to write any CSS.",
+    is_opened: false
+  },
+  {
+    title: "What is Tailwind CSS?",
+    text: "Tailwind CSS is a CSS framework like Bootstrap, that speeds up development time for developers. Tailwind CSS uses classes to style elements, meaning you do not have to write any CSS.",
+    is_opened: false
+  },
+  {
+    title: "What is Tailwind CSS?",
+    text: "Tailwind CSS is a CSS framework like Bootstrap, that speeds up development time for developers. Tailwind CSS uses classes to style elements, meaning you do not have to write any CSS.",
+    is_opened: false
+  }
+]);
 </script>
 
 <style lang="scss">
@@ -56,7 +86,8 @@
     font-size: font-calculate(72px, 2.4);
     line-height: calc(81 / 72 * 100%);
   }
-  &__content{
+
+  &__content {
     display: flex;
     align-items: stretch;
     flex-direction: column;
@@ -69,6 +100,14 @@
   justify-content: flex-start;
   flex-direction: column;
 
+  &.opened &{
+    &__content {
+    display: block;
+    }
+    &__arrow{
+      transform: rotate(90deg);
+  }}
+
   &__button {
     font-family: 'IBM Plex Sans', sans-serif;
     font-style: normal;
@@ -79,10 +118,24 @@
     padding: viewport-calculate($notebook_start, $phone, 30px, 10px) 0;
     text-align: left;
     border-bottom: 0.9px solid $gray-500;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-    &:first-of-type{
+    &:first-of-type {
       border-top: 0.9px solid $gray-500;
     }
+  }
+
+  &__content {
+    display: none;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: font-calculate(25.2px);
+    line-height: 140%;
+    color: $gray-300;
+    padding: viewport-calculate($notebook_start, $phone, 34.2px, 19px) 0;
   }
 }
 </style>
